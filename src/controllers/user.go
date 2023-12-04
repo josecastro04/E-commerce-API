a2e6db61e2581b6e4572d82161b22c343b89b9e6
José Castro <bychozking@gmail.com>
2023-12-04 20:41:15 +0000

    User-related functions

--- src/controllers/user.go
+++ src/controllers/user.go
@@ -6,10 +6,55 @@ import (
 	"api/src/models"
 	"api/src/repositories"
 	"api/src/responses"
+	"api/src/security"
 	"encoding/json"
 	"io"
 	"net/http"
 )
+
+func LoginUser(w http.ResponseWriter, r *http.Request) {
+	bodyRequest, err := io.ReadAll(r.Body)
+	if err != nil {
+		responses.Erro(w, http.StatusBadRequest, err)
+		return
+	}
+
+	var user models.User
+
+	if err = json.Unmarshal(bodyRequest, &user); err != nil {
+		responses.Erro(w, http.StatusBadRequest, err)
+		return
+	}
+
+	db, err := database.ConnectWithDatabase()
+
+	if err != nil {
+		responses.Erro(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	repository := repositories.NewRepositoryUser(db)
+
+	userInfo, err := repository.SearchUserByEmail(user.Email)
+
+	if err != nil {
+		responses.Erro(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	if err = security.CompareHashWithPassword(user.Password, userInfo.Password); err != nil {
+		responses.Erro(w, http.StatusUnauthorized, err)
+		return
+	}
+
+	token, err := authentication.CreateToken(userInfo.ID)
+	if err != nil {
+		responses.Erro(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	responses.JSON(w, http.StatusOK, token)
+}
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
 	bodyRequest, err := io.ReadAll(r.Body)
@@ -68,4 +113,75 @@ func ShowUserInfo(w http.ResponseWriter, r *http.Request) {
 	}
 
 	responses.JSON(w, http.StatusOK, user)
+}
+
+func UpdatePassword(w http.ResponseWriter, r *http.Request) {
+	userID, err := authentication.ExtractUserID(r)
+	if err != nil {
+		responses.Erro(w, http.StatusBadRequest, err)
+		return
+	}
+
+	bodyRequest, err := io.ReadAll(r.Body)
+	if err != nil {
+		responses.Erro(w, http.StatusBadRequest, err)
+		return
+	}
+
+	var password models.Password
+
+	if err = json.Unmarshal(bodyRequest, &password); err != nil {
+		responses.Erro(w, http.StatusBadRequest, err)
+		return
+	}
+
+	db, err := database.ConnectWithDatabase()
+
+	if err != nil {
+		responses.Erro(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	repository := repositories.NewRepositoryUser(db)
+
+	hash, err := repository.SearchPasswordFromUser(userID)
+	if err != nil {
+		responses.Erro(w, http.StatusInternalServerError, err)
+	}
+
+	if err = security.CompareHashWithPassword(password.Current, hash); err != nil {
+		responses.Erro(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if err = repository.UpdateUserInfo(userID, password); err != nil {
+		responses.Erro(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	responses.JSON(w, http.StatusOK, "Updated")
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	userID, err := authentication.ExtractUserID(r)
+	if err != nil {
+		responses.Erro(w, http.StatusBadRequest, err)
+		return
+	}
+
+	db, err := database.ConnectWithDatabase()
+
+	if err != nil {
+		responses.Erro(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	repository := repositories.NewRepositoryUser(db)
+
+	if err = repository.Delete(userID); err != nil {
+		responses.Erro(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	responses.JSON(w, http.StatusOK, "Deleted")
 }
