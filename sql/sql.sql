45fcf1ad49a72707d336c595ea459f73c0e1d4be
José Castro <bychozking@gmail.com>
2023-12-05 22:18:07 +0000

    database commands

--- sql/sql.sql
+++ sql/sql.sql
@@ -1,11 +1,20 @@
-drop table produtos;
-drop table carrinho;
-drop table user;
+drop table if exists user;
+drop table if exists product;
 
 create table user (
     id int primary key auto_increment,
     name varchar(50) not null,
     email varchar(50) not null unique,
-    password varchar(50) not null,
+    password varchar(100) not null,
+    roletype varchar(20) not null,
     created_in timestamp default current_timestamp()
+)ENGINE=INNODB;
+
+create table product (
+    id int primary key auto_increment,
+    name varchar(100) not null,
+    description varchar(500) not null,
+    price float(8,2) not null,
+    stock int not null,
+    added_in timestamp default current_timestamp()
 )ENGINE=INNODB;
