024c7b5a2957a9d4f9b385c57744ffd30e86a67d
José Castro <bychozking@gmail.com>
2023-12-19 01:35:38 +0000

    Order models

--- src/models/order.go
+++ src/models/order.go
@@ -4,13 +4,13 @@ import "time"
 
 type OrderItem struct {
 	Product Product `json:"Product"`
-	OrderID uint64  `json:"OrderID"`
+	OrderID string  `json:"OrderID"`
 	Amount  uint64  `json:"Amount"`
 	Price   float64 `json:"Price"`
 }
 
 type Order struct {
-	OrderID    uint64      `json:"OrderID"`
+	OrderID    string      `json:"OrderID"`
 	UserID     string      `json:"UserID"`
 	OrderItems []OrderItem `json:"OrderItems"`
 	Date       time.Time   `json:"Date"`
